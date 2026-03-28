cb539c7b7ed68933814d331acab37cd81fdffdaa
syed292-92 <syed292@my.yorku.ca>
2026-03-27 21:36:50 -0400

    Added to Flip.java

--- src/Pixel Craft/src/Flip.java
+++ src/Pixel Craft/src/Flip.java
@@ -5,6 +5,18 @@ public class Flip extends Converter{
     // 1. Convert Method Overridden
     @Override
     public void convert(String input, String output) throws IOException{
+        // 1. Load Image
         BufferedImage image = loadImage(input);
+
+        // 2. Nested for loop
+        for(int x = 0; x < image.getWidth(); x++){
+            for(int y = 0; y < image.getHeight(); y++){
+
+            }
+        }
+
+        // 3. Save Image
+
+
     }
 }
