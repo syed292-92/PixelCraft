cc3345fbe30a35a2eb82e6a246fdeb0a9f3021de
syed292-92 <syed292@my.yorku.ca>
2026-03-28 02:40:34 -0400

    Finished Flip and Solarize Files

--- src/Pixel Craft/src/Flip.java
+++ src/Pixel Craft/src/Flip.java
@@ -10,13 +10,18 @@ public class Flip extends Converter{
 
         // 2. Nested for loop
         for(int x = 0; x < image.getWidth(); x++){
-            for(int y = 0; y < image.getHeight(); y++){
+            for(int y = 0; y < image.getHeight() / 2; y++){
+                // a. get RGB Values
+                int top = image.getRGB(x, y);
+                int bottom = image.getRGB(x, image.getHeight() - 1 - y);
 
+                // b. set RGB Values
+                image.setRGB(x, y, bottom);
+                image.setRGB(x, image.getHeight() - 1 - y, top);
             }
         }
 
         // 3. Save Image
-
-
+        saveImage(image, output);
     }
 }
