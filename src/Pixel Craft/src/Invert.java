417f940243a07241e1b1dea1de81ee24848ce9b7
syed292-92 <syed292@my.yorku.ca>
2026-03-27 20:48:54 -0400

    Finished invert.java

--- src/Pixel Craft/src/Invert.java
+++ src/Pixel Craft/src/Invert.java
@@ -5,6 +5,22 @@ public class Invert extends Converter{
     // 1. Convert Method Overridden
     @Override
     public void convert(String input, String output) throws IOException{
+        // A. Load Image
         BufferedImage image = loadImage(input);
+
+        // a. Iterate through each y level (bottom to top)
+        for(int y = 0; y < image.getHeight(); y++){
+            // b. iterate through each pixel (left to right)
+            for(int x = 0; x < image.getWidth(); x++){
+                // c. Get the ARGB of current pixel
+                ARGB p = new ARGB(image.getRGB(x, y));
+
+                // d. Set inverted RGB
+                image.setRGB(x, y, new ARGB(p.alpha, 255 - p.red, 255 - p.green, 255 - p.blue).toInt());
+            }
+        }
+
+        // B. Save Image
+        saveImage(image, output);
     }
 }
