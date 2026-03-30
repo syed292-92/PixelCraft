d85fa879a6cfd2e8e6c77d20343576ded6f52de4
syed292-92 <syed292@my.yorku.ca>
2026-03-30 18:37:02 -0400

    Create Flip.java

---
+++ src/Flip.java
@@ -0,0 +1,34 @@
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Flip extends Converter{
+    /*
+    * To run class:
+    * javac -d bin src/*.java
+    * java -cp bin PixelCraft Flip toronto.png
+    */
+
+
+    // 1. Convert Method Overridden
+    @Override
+    public void convert(String input, String output) throws IOException{
+        // 1. Load Image
+        BufferedImage image = loadImage(input);
+
+        // 2. Nested for loop
+        for(int x = 0; x < image.getWidth(); x++){
+            for(int y = 0; y < image.getHeight() / 2; y++){
+                // a. get RGB Values
+                int top = image.getRGB(x, y);
+                int bottom = image.getRGB(x, image.getHeight() - 1 - y);
+
+                // b. set RGB Values
+                image.setRGB(x, y, bottom);
+                image.setRGB(x, image.getHeight() - 1 - y, top);
+            }
+        }
+
+        // 3. Save Image
+        saveImage(image, output);
+    }
+}
